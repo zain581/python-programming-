7ffba8a00d2823e166c99a771072dfddf1a34b7c
zain581 <az4860364@gmail.com>
2022-10-02 10:10:15 -0400

    test

---
+++ leetcode/test.py
@@ -0,0 +1,13 @@
+def main():
+    return "test"+"pask"
+def test(string1):
+    if len(string1)==0:
+        return 0
+    return 1
+if __name__=="__main__":
+    print(test(input("enter the string")))
+
+
+
+
+
