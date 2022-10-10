2be8c5fd8debcda33e696e02da1de53f17969ea7
zain581 <az4860364@gmail.com>
2022-10-10 10:10:39 -0400

    good path

---
+++ leetcode/good_path_tree.py
@@ -0,0 +1,10 @@
+from tree import node
+from tree import inorder
+def number_of_path(root,counter):
+    print()
+if __name__=="__main__":
+    n=node(10)
+    n.insert(1)
+    n.insert(1)
+    res=[]
+    inorder(n,res)
